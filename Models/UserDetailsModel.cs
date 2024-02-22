95b0bbc52cf24310789d2b13987af32f63fed20f
Sonja Perković <soperkov@vsite.hr>
2024-02-22 11:54:00 +0100

    small bug fixes, design changes

--- Models/UserDetailsModel.cs
+++ Models/UserDetailsModel.cs
@@ -9,9 +9,15 @@ namespace Nutrition.Models
         public int UserId { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
+
+        [RegularExpression(@"^(?!0+(\.0+)?$)\d+(\.\d+)?$", ErrorMessage = "Mora biti uneseno.")]
         public double Height { get; set; }
+
+        [RegularExpression(@"^(?!0+(\.0+)?$)\d+(\.\d+)?$", ErrorMessage = "Mora biti uneseno.")]
         public double Weight { get; set; }
+
+        [RegularExpression(@"^(?!0+(\.0+)?$)\d+(\.\d+)?$", ErrorMessage = "Mora biti uneseno.")]
         public double Years { get; set; }
-        public string Sex { get; set; } 
+        public string Sex { get; set; }
     }
 }
